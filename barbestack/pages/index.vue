4caf374c2bcb9a1e57d84f7557ea4b70382158f0
Nanigashi <b1022054@fun.ac.jp>
2024-12-10 18:53:06 +0900

    カメラ機能実装テスト

--- barbestack/pages/index.vue
+++ barbestack/pages/index.vue
@@ -1,5 +1,27 @@
 <template>
-    <div>
-        
-    </div>
+  <video ref="video" autoplay></video>
 </template>
+
+<script setup>
+import { ref } from 'vue';
+import { useUserMedia } from '@vueuse/core';
+
+const video = ref(null);
+
+const { start, stop } = useUserMedia({
+  audio: false,
+  video: {
+    facingMode: { exact: 'environment' } // Request rear camera by default
+  }
+});
+
+start().then(stream => {
+  video.value.srcObject = stream;
+  video.value.onloadedmetadata = () => {
+    video.value.play();
+  };
+}).catch(error => {
+  console.error('Error accessing camera:', error);
+  // Optionally display a user-friendly error message or retry logic
+});
+</script>
